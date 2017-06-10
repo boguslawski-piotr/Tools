208172413d327b785d61fa95c34b4743f9020b30
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-10 17:31:02 -0400

    First stable and really useful version

--- Versions/Program.cs
+++ Versions/Program.cs
@@ -1,130 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json;
 
 namespace Versions
 {
 	class MainClass
 	{
-		class Version
-		{
-			public string FileName;
-			public List<string> Items;
-
-			public int MajorGroup = 0;
-			public int MinorGroup = 1;
-			public int BuildGroup = 2;
-			public int RevisionGroup = 3;
-		};
-
-		enum Number
-		{
-			Major,
-			Minor,
-			Build,
-			Revision,
-		}
-
 		public static void Main(string[] args)
 		{
-			Number whichNumber = Number.Revision;
 			foreach (var arg in args)
 			{
-				if (arg.StartsWith("--", StringComparison.Ordinal))
+				if (arg == "--help" || arg == "--?")
 				{
-					if (arg == "--help")
-						DisplayHelp();
-					else
-					{
-						if (!Enum.TryParse<Number>(arg.Substring(2), true, out whichNumber))
-							Console.WriteLine($"Incorrect parameter: {arg}");
-					}
-				}
-				else
-				{
-					Console.WriteLine($"Loading: {arg} to process: {whichNumber}");
-					try
-					{
-						using (StreamReader s = File.OpenText(arg))
-						{
-							string json = s.ReadToEnd();
-							foreach (Version v in JsonConvert.DeserializeObject<List<Version>>(json))
-								Process(v, whichNumber);
-						}
-					}
-					catch (Exception ex)
-					{
-						Console.Error.WriteLine("  " + ex.Message);
-						continue;
-					}
-
-					Console.WriteLine("");
+					DisplayHelp();
+					return;
 				}
 			}
-		}
 
-		static void Process(Version v, Number whichNumber)
-		{
-			Console.WriteLine($"  Processing: {whichNumber} in: {v.FileName}");
-
-			int groupToProcess = -1;
-			if (whichNumber == Number.Major) groupToProcess = v.MajorGroup;
-			if (whichNumber == Number.Minor) groupToProcess = v.MinorGroup;
-			if (whichNumber == Number.Build) groupToProcess = v.BuildGroup;
-			if (whichNumber == Number.Revision) groupToProcess = v.RevisionGroup;
-			if (groupToProcess == -1)
-			{
-				Console.WriteLine($"    {whichNumber} has no assigned group");
-				return;
-			}
-
-			string d = null;
-			using (StreamReader reader = File.OpenText(v.FileName))
-				d = reader.ReadToEnd();
-
-			foreach (var pattern in v.Items)
-			{
-				Match m = Regex.Match(d, pattern);
-				if (!m.Success)
-				{
-					Console.WriteLine($"    No match for: {pattern}");
-					continue;
-				}
-
-				Console.WriteLine($"    Found: {m.ToString()}");
-
-				int currentGroup = 0;
-				foreach (Group g in m.Groups)
-				{
-					// first group equals to whole match
-					if (currentGroup > 0 && currentGroup - 1 == groupToProcess)
-					{
-						if (int.TryParse(g.ToString(), out int n))
-						{
-							Console.WriteLine($"      For group: {currentGroup - 1} changing: {n} to {n + 1}");
-
-							n++;
-							string ds = d.Substring(0, g.Index);
-							string de = d.Substring(g.Index + g.Length);
-							d = ds + n.ToString() + de;
-
-							using (StreamWriter writer = File.CreateText(v.FileName))
-								writer.Write(d);
-						}
-						else
-							Console.WriteLine($"      Group {currentGroup}: has incorrect number: {g.ToString()}");
-					}
-
-					currentGroup++;
-				}
-			}
+			Versions v = new Versions(Console.Out, Console.Error);
+			v.Process(args);
 		}
 
 		static void DisplayHelp()
 		{
-			Console.WriteLine("Help...");
+			Console.WriteLine("Help..."); // TODO: write nice help...
 		}
 	}
 }
---
+++ Versions/Versions.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace Versions
+{
+	public class Versions
+	{
+		TextWriter _msgOut;
+		TextWriter _errOut;
+
+		class File
+		{
+			public string FileName;
+			public List<string> FileNames;
+			public List<Entry> Entries;
+		};
+
+		class Entry
+		{
+			public string Pattern;
+			public int MajorGroup = 0;
+			public int MinorGroup = 1;
+			public int BuildGroup = 2;
+			public int RevisionGroup = 3;
+		};
+
+		enum Number
+		{
+			Major,
+			Minor,
+			Build,
+			Revision,
+		}
+
+		enum Operation
+		{
+			Inc,
+			Dec,
+			Set,
+			Add,
+			Sub,
+		};
+
+		public Versions(TextWriter msgOut, TextWriter errOut)
+		{
+			_msgOut = msgOut;
+			_errOut = errOut;
+		}
+
+		public void Process(string[] args)
+		{
+			Number currentNumber = Number.Revision;
+			Operation currentOperation = Operation.Inc;
+			string currentValue = null;
+			bool nextShouldBeValue = false;
+
+			foreach (var arg in args)
+			{
+				if (arg.StartsWith("--", StringComparison.Ordinal))
+				{
+					if (!Enum.TryParse<Number>(arg.Substring(2), true, out Number number))
+					{
+						if (!Enum.TryParse<Operation>(arg.Substring(2), true, out Operation operation))
+							_errOut.WriteLine($"Incorrect parameter: {arg}");
+						else
+						{
+							if (operation > Operation.Dec) nextShouldBeValue = true;
+							currentOperation = operation;
+						}
+					}
+					else
+						currentNumber = number;
+				}
+				else
+				{
+					if (nextShouldBeValue)
+					{
+						currentValue = arg;
+						nextShouldBeValue = false;
+					}
+					else
+					{
+						string currentValueDesc = (currentOperation == Operation.Set) ? "to " + currentValue : null;
+						_msgOut.WriteLine($"Loading: {Path.GetFullPath(arg)} to process: {currentNumber} {currentOperation} {currentValueDesc}");
+						try
+						{
+							using (StreamReader s = System.IO.File.OpenText(arg))
+							{
+								string json = s.ReadToEnd();
+								string path = Path.GetDirectoryName(arg);
+								foreach (File v in JsonConvert.DeserializeObject<List<File>>(json))
+								{
+									try
+									{
+										Process(path, v, currentNumber, currentOperation, currentValue);
+									}
+									catch (FileNotFoundException ex)
+									{
+										_errOut.WriteLine(ex.Message);
+										continue;
+									}
+								}
+							}
+						}
+						catch (Exception ex)
+						{
+							_errOut.WriteLine(ex.Message);
+							continue;
+						}
+
+						Console.WriteLine("");
+					}
+				}
+			}
+		}
+
+		void Process(string path, File v, Number number, Operation operation, string value)
+		{
+			List<string> fileNames = v.FileNames;
+			if (fileNames == null)
+			{
+				fileNames = new List<string>() {
+					v.FileName,
+				};
+			}
+
+			foreach (var vfn in fileNames)
+			{
+				string fileName = Path.GetFullPath(Path.Combine(path, vfn));
+				_msgOut.WriteLine($"Processing: {fileName}");
+
+				string d = null;
+				using (StreamReader reader = System.IO.File.OpenText(fileName))
+					d = reader.ReadToEnd();
+
+				foreach (var entry in v.Entries)
+				{
+					int groupToProcess = -1;
+					if (number == Number.Major) groupToProcess = entry.MajorGroup;
+					if (number == Number.Minor) groupToProcess = entry.MinorGroup;
+					if (number == Number.Build) groupToProcess = entry.BuildGroup;
+					if (number == Number.Revision) groupToProcess = entry.RevisionGroup;
+					if (groupToProcess == -1)
+					{
+						_msgOut.WriteLine($"Pattern: {entry.Pattern} has no assigned group for: {number}");
+						continue;
+					}
+
+					Match m = Regex.Match(d, entry.Pattern);
+					if (!m.Success)
+					{
+						_msgOut.WriteLine($"No match for pattern: {entry.Pattern}");
+						continue;
+					}
+
+					_msgOut.WriteLine($"Found: {m.ToString()}");
+
+					int currentGroup = 0;
+					foreach (Group g in m.Groups)
+					{
+						// first group equals to whole match
+						if (currentGroup > 0 && currentGroup - 1 == groupToProcess)
+						{
+							if (operation != Operation.Set)
+							{
+								if (int.TryParse(g.ToString(), out int n))
+								{
+									if (operation == Operation.Inc)
+										n++;
+									else
+										n--;
+									value = n.ToString();
+								}
+								else
+								{
+									_errOut.WriteLine($"Group {currentGroup - 1}: has incorrect number: {g.ToString()}");
+									value = null;
+								}
+							}
+
+							if (value != null)
+							{
+								_msgOut.WriteLine($"For group: {currentGroup - 1} changing: {g.ToString()} to {value}");
+
+								string ds = d.Substring(0, g.Index);
+								string de = d.Substring(g.Index + g.Length);
+								d = ds + value + de;
+
+								using (StreamWriter writer = System.IO.File.CreateText(fileName))
+									writer.Write(d);
+							}
+						}
+
+						currentGroup++;
+					}
+				}
+			}
+		}
+	}
+}
